0aaca88b8b0e7d8019d1ed89d740eab92157d1cf
zanatooo <m8elzanaty@gmail.com>
2022-04-10 04:00:52 -0700

    adding game-html and game-css

---
+++ js/game.js
@@ -0,0 +1,7 @@
+const question = document.querySelector('#question');
+const choices = document.querySelector('.choice-text');
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarfull');
+
+console.log("hello world");
