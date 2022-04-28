024e2b3e79ff2f5c0d45cbcda90a130f28174195
zanatooo <m8elzanaty@gmail.com>
2022-04-27 20:26:01 -0700

    fixing more links

--- end.js
+++ end.js
@@ -35,7 +35,7 @@ highScores.splice(5);
 
 localStorage.setItem('highScores', JSON.stringify(highScores));
 
-window.location.assign('/');
+window.location.assign('./');
 console.log(highScores);
 
 }
